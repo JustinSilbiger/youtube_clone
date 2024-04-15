b9a27c936c8e20a59282df6d651b1d590c0a2bdf
shabbat-shalom <justinsilb4@gmail.com>
2024-04-15 13:55:33 -0400

    youtube_clone Home.js, Layout.js, Header.js

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import Layout from "./components/Layout";
+
+const Home = () => {
+    return(
+        <Layout>
+            <>
+                <h1>Hello YouTube!</h1>
+            </> 
+        </Layout>      
+    );
+
+};
+
+export default Home;
---
+++ src/components/SearchResults.js
@@ -0,0 +1,18 @@
+import Layout from "./Layout";
+
+const SearchResults = () => {
+
+
+    return (
+
+        <Layout>
+            <>
+            <h1>
+                Search Results
+            </h1>
+            </>
+        </Layout>
+    )
+}
+
+export default SearchResults;
